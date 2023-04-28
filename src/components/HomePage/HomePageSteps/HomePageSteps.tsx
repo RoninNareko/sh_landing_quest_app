32a28f58ba2dff39a686140db8949dab211a77fe
Arpine <a.aleksanyan@diagnocat.com>
2023-04-28 18:02:45 +0400

    steps to do

--- src/components/HomePage/HomePageSteps/HomePageSteps.tsx
+++ src/components/HomePage/HomePageSteps/HomePageSteps.tsx
@@ -1,5 +1,6 @@
 import styles from "./HomePageSteps.module.scss";
 import classNames from "classnames";
+import Steps from "./Steps/Steps";
 
 export default function HomePageSteps() {
   const cx = classNames.bind(styles);
@@ -10,7 +11,13 @@ export default function HomePageSteps() {
           <h2 className={cx(styles.headerText)}>Steps</h2>
         </div>
       </header>
-      <section className={cx(styles.stepsContext)}>asdasd</section>
+      <section className={cx(styles.stepsContext)}>
+        <section>
+          <Steps />
+        </section>
+        <section>line</section>
+        <section></section>
+      </section>
     </>
   );
 }
---
+++ src/components/HomePage/HomePageSteps/Steps/Steps.constants.ts
@@ -0,0 +1,5 @@
+import stepsVector from "@/assets/images/stepsVector.png";
+
+export const STEPS_RIGHT_BACKGROUND_VECTOR_STYLE = {
+  backgroundImage: `url(${stepsVector.src})`,
+};
